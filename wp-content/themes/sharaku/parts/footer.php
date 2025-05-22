8029d997ed0ba446d909f721291e06c5882e75b7
UTakuto <taku.0307.ue@icloud.com>
2025-05-22 14:37:05 +0900

    footer.phpにフッターのHTML構造を追加し、wp_footer関数を呼び出す位置を設定

---
+++ wp-content/themes/sharaku/parts/footer.php
@@ -0,0 +1,14 @@
+
+
+    <footer>
+        <p>
+            <small>
+                &copy; sharaku 2025
+            </small>
+        </p>
+    </footer>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
