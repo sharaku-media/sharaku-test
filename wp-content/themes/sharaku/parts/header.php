ffe5b481f4016f8fbf0d48524dd05c4d1b58fda8
UTakuto <taku.0307.ue@icloud.com>
2025-05-22 14:37:08 +0900

    header.phpファイルを新規作成し、基本的なHTML構造を追加

---
+++ wp-content/themes/sharaku/parts/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo("title"); ?></title>
+    <!-- 全体共通style -->
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/styles/index.css">
+</head>
+<body>
+    <?php get_header(); ?>
+    <!-- ここの下にheaderを用意 -->
