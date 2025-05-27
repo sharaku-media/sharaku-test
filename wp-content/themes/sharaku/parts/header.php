3d5425e12acb019ee681bd010c7105f4e7264ba4
UTakuto <taku.0307.ue@icloud.com>
2025-05-27 10:05:57 +0900

    index title , descriptionのwordpress化

--- wp-content/themes/sharaku/parts/header.php
+++ wp-content/themes/sharaku/parts/header.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php bloginfo("title") ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <!-- <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style/ress.css"> -->
     <?php wp_head() ?>
 </head>
 <body>
