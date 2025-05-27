4d829b9e549114a638b9bbd976343cb339901fea
UTakuto <taku.0307.ue@icloud.com>
2025-05-27 14:55:06 +0900

    Google Fontsを追加し、スタイルの読み込みを改善

--- wp-content/themes/sharaku/parts/header.php
+++ wp-content/themes/sharaku/parts/header.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo("title"); ?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    <!-- 全体共通style -->
-    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/styles/index.css">
-    <?php get_header(); ?>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <!-- 共通 style -->
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/styles/ress.css">
+    <?php wp_head() ?>
 </head>
 <body>
     <!-- ここの下にheaderを用意 -->
