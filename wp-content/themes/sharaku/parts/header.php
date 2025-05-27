4ace322675ac85da0bfec8cae5bfca1323c5d41a
UTakuto <taku.0307.ue@icloud.com>
2025-05-27 14:55:35 +0900

    修正

--- wp-content/themes/sharaku/parts/header.php
+++ wp-content/themes/sharaku/parts/header.php
@@ -7,8 +7,8 @@
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
     <!-- 共通 style -->
     <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/styles/ress.css">
     <?php wp_head() ?>
