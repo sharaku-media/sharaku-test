8880ffaa2ee0061ecbcb263ed3faca3e42841ec5
UTakuto <taku.0307.ue@icloud.com>
2025-05-27 10:31:06 +0900

    ress.cssのスタイルシートリンクを修正

--- wp-content/themes/sharaku/parts/header.php
+++ wp-content/themes/sharaku/parts/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo("title") ?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    <!-- <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style/ress.css"> -->
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/styles/ress.css">
     <?php wp_head() ?>
 </head>
 <body>
