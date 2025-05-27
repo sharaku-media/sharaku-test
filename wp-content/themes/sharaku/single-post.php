d8c5de9a8e80058da6d15414b45ab40fd854c134
UTakuto <taku.0307.ue@icloud.com>
2025-05-27 10:31:01 +0900

    投稿タイトルの表示を復元し、タグリストを追加

--- wp-content/themes/sharaku/single-post.php
+++ wp-content/themes/sharaku/single-post.php
@@ -1,16 +1,30 @@
+<?php
+    $tags = get_the_tags();
+?>
 <?php include get_template_directory() . '/parts/header.php'; ?>
-    <!-- <h1><?php the_title() ?></h1> -->
-        <!-- <?php
+    <h1><?php the_title() ?></h1>
+        <div>
+            <?php if( $tags ){
+                echo '<ul class="tag-list">';
+                    foreach( $tags as $tag ){
+                        echo '<li>';
+                        echo '<p class="tag-button">' . esc_html( $tag->name ) . '</p>';
+                        echo '</li>';
+                    }
+                echo '</ul>';
+                } 
+            ?>
+        </div>
+        <?php
         // WordPressのループを開始
         if ( have_posts() ) :
             while ( have_posts() ) : the_post();
-                // 投稿のタイトルを表示
-                the_title( '<h2>', '</h2>' );
                 // 投稿のコンテンツを表示
                 the_content();
             endwhile;
         else :
             echo '<p>投稿が見つかりませんでした。</p>';
         endif;
-        ?> -->
+        ?>
+        
 <?php include get_template_directory() . '/parts/footer.php'; ?>
