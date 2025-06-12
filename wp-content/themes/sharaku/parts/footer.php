670c4dcf82225d2d37137093fe6d44c54b37626f
UTakuto <taku.0307.ue@icloud.com>
2025-06-12 11:49:12 +0900

    footer.phpから不要な<footer>セクションを削除

--- wp-content/themes/sharaku/parts/footer.php
+++ wp-content/themes/sharaku/parts/footer.php
@@ -1,10 +1,3 @@
-    <footer>
-        <p>
-            <small>
-                &copy; sharaku 2025
-            </small>
-        </p>
-    </footer>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCR_Nu28owuza4O4zP-LZUMMIelifwVP5g"></script>
     <?php wp_footer(); ?>
 </body>
