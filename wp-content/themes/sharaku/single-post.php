3d147abdedfab3b541f9dc75f39f25f4c1553aa0
UTakuto <taku.0307.ue@icloud.com>
2025-06-26 12:12:07 +0900

    トップに戻るボタンを追加

--- wp-content/themes/sharaku/single-post.php
+++ wp-content/themes/sharaku/single-post.php
@@ -3,6 +3,14 @@
 ?>
 <?php include get_template_directory() . '/parts/header.php'; ?>
 <main class="single-post">
+    <div class="back-to-top">
+        <a href="<?php echo home_url(); ?>" class="back-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20z" />
+            </svg>
+            トップに戻る
+        </a>
+    </div>
     <div class="main-images">
         <div class="image-track">
             <?php
